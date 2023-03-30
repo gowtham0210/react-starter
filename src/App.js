5a6a18a916c70eb0c4f24fc31274e63498184817
Gowtham <49744195+gowtham0210@users.noreply.github.com>
2023-03-30 21:42:04 +0530

    1st commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <p className="text-3xl text-red font-bold underline">
+       Hello world!
+      </p>
+    </div>
+  );
+}
+
+export default App;
